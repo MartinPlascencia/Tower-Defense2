20f1fc2e801e80f556c72870e03192d96fd2817f
MartinPlascencia <martin.plascencia@live.com.mx>
2025-07-16 20:33:27 -0600

    Implemented coins, added enemy.

--- Assets/Scripts/Rotate.cs
+++ Assets/Scripts/Rotate.cs
@@ -6,6 +6,6 @@ public class Rotate : MonoBehaviour
     private Vector3 _rotationSpeed = new Vector3(0, 10f, 0);
     void Update()
     {
-        transform.Rotate(_rotationSpeed * Time.deltaTime);
+        transform.Rotate(_rotationSpeed * Time.deltaTime, Space.Self);
     }
 }
