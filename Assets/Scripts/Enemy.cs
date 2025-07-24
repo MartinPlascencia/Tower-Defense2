b3a983efc1cadbd47b915dbf8fb7216652d3e42e
MartinPlascencia <martin.plascencia@live.com.mx>
2025-07-23 20:56:27 -0600

    Implemented Gun

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -11,21 +11,22 @@ public class Enemy : MonoBehaviour
     [SerializeField]
     private UnityEvent _onInitialize;
     private bool _isRunning;
-    private Transform _targetTransform;
+    private Vector3 _targetPosition;
     private Health _targetHealth;
-    private Coroutine _attackCoroutine;
     private void OnEnable()
     {
         _isRunning = false;
         _onInitialize?.Invoke();
-        GetTarget();
+        Invoke("GetTarget", 0.5f);
     }
     private void GetTarget()
     {
         GameObject target = GameObject.FindGameObjectWithTag(_enemyData.primaryTargetTag);
         if (target != null && !_isRunning)
         {
-            _targetTransform = target.transform;
+            Vector3 targetPosition = new Vector3(target.transform.position.x,
+                transform.position.y, target.transform.position.z);
+            _targetPosition = targetPosition;
             _targetHealth = target.GetComponent<Health>();
             _isRunning = true;
             _animator.Play(_enemyData.runAnimationName);
@@ -35,12 +36,13 @@ public class Enemy : MonoBehaviour
     {
         if (_isRunning)
         {
-            transform.position = Vector3.MoveTowards(transform.position, _targetTransform.position, _enemyData.runSpeed * Time.deltaTime);
-            transform.LookAt(_targetTransform.position);
-            if (Vector3.Distance(transform.position, _targetTransform.position) <= _enemyData.attackRange)
+            transform.position = Vector3.MoveTowards(transform.position, _targetPosition,
+                _enemyData.runSpeed * Time.deltaTime);
+            transform.LookAt(_targetPosition);
+            if (Vector3.Distance(transform.position, _targetPosition) <= _enemyData.attackRange)
             {
                 _isRunning = false;
-                _attackCoroutine = StartCoroutine(Attack());
+                StartCoroutine(Attack());
             }
         }
     }
@@ -49,6 +51,7 @@ public class Enemy : MonoBehaviour
         while (_targetHealth != null && _targetHealth.CurrentHealth > 0)
         {
             _animator.Play(_enemyData.attackAnimationName, 0, 0f);
+            SoundManager.instance.Play(_enemyData.attackSoundName);
             yield return new WaitForSeconds(_enemyData.attackDuration);
             if (_targetHealth != null)
             {
@@ -56,20 +59,28 @@ public class Enemy : MonoBehaviour
             }
             yield return new WaitForSeconds(_enemyData.attackCooldown);
         }
-        _targetHealth = null;
-        _targetTransform = null;
-        GetTarget();
+        Win();
     }
-
+    private void Win()
+    {
+        _animator.Play(_enemyData.winAnimationName);
+    }
+    public void Die()
+    {
+        StopAllCoroutines();
+        _isRunning = false;
+        StartCoroutine(DieCoroutine());
+    }
+    private IEnumerator DieCoroutine()
+    {
+        _animator.Play(_enemyData.dieAnimationName);
+        yield return new WaitForSeconds(2f);
+        gameObject.SetActive(false);
+    }
     private void OnDisable()
     {
-        if (_attackCoroutine != null)
-        {
-            StopCoroutine(_attackCoroutine);
-            _attackCoroutine = null;
-        }
+        StopAllCoroutines();
         _isRunning = false;
-        _targetTransform = null;
         _targetHealth = null;
     }
 
